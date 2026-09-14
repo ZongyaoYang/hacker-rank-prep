16c0b367dff7bd15fea039d32ff7fa654467633c
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-13 22:24:58 -0600

    add counter app

--- src/pages/counter/Counter.jsx
+++ src/pages/counter/Counter.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const Counter = () => {
-  return <h3>This is Counter Component</h3>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    if (count > 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <div>
+      <span data-testid="count">{count}</span>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 };
 
 export default Counter;
