5d542bfd0077495fd86a3c18ad1856b101269913
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-13 23:16:09 -0600

    complete controlled form

--- src/pages/controlled-input-form/ControlledForm.jsx
+++ src/pages/controlled-input-form/ControlledForm.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 const ControlledForm = () => {
-    return <h3>This is ControlledForm Component</h3>
-}
+  const [text, setText] = useState("");
+  const [submitted, setSubmitted] = useState("");
+
+  const onType = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() !== "") {
+      setSubmitted(text);
+      setText("");
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={text} onChange={onType} />
+      <button>Submit</button>
+      <p>{submitted}</p>
+    </form>
+  );
+};
 
 export default ControlledForm;
